079d92ca4f6ac7dd8307bddfcf110efa5741c92e
AshenWan <167110410+AshenWan@users.noreply.github.com>
2024-06-01 16:34:46 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const searchForm = document.querySelector(".search-form");
+const navbar = document.querySelector(".navbar");
+
+//! buttons
+const searchBtn = document.querySelector("#search-btn");
+
+
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(searchBtn) &&
+      !e.composedPath().includes(searchForm)
+    ) {
+      searchForm.classList.remove("active");
+    }
+  });
+});
+
+
+
